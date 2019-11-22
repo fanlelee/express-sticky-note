c68b08274cf81e9695866259c14376c8d4e44720
fanlelee <529378687@qq.com>
2019-11-22 23:07:22 +0800

    tst

--- app.js
+++ app.js
@@ -19,14 +19,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(session({secret: 'sessionsecret'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
--- routes/auth.js
+++ routes/auth.js
@@ -34,14 +34,6 @@ passport.use(new GitHubStrategy({
   }
 ))
 
-router.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
 
 router.get('/github',   
     passport.authenticate('github'),
