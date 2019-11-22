1377e2dfd905bb12bcb3319cea3187cf6cb98b6a
fanlelee <529378687@qq.com>
2019-11-22 22:49:30 +0800

    test

--- routes/auth.js
+++ routes/auth.js
@@ -34,8 +34,20 @@ passport.use(new GitHubStrategy({
   }
 ))
 
+router.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 router.get('/github',   
-    passport.authenticate('github')
+    passport.authenticate('github'),
+    function(req,res,next){
+        console.log('gitgit')
+    }
 )
 router.get('/github/callback',   
     passport.authenticate('github', { failureRedirect: '/' }),
