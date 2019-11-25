396e1fb387832523ce1bc4f6a2eeee71f7b364b9
fanlelee <529378687@qq.com>
2019-11-25 16:40:03 +0800

    t

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,7 +1,7 @@
 
-$('.github-login').on('click', function(){
-    $.get('/auth/github')
-})
+// $('.github-login').on('click', function(){
+//     $.get('/auth/github')
+// })
 
 var NoteManager = require('mod/note-manager.js')
 var Event = require('mod/event.js')
